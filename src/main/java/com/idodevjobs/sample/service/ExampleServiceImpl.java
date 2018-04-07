618af2c193aa16f0a69f1993b7632868b01fc8cf
RACHIT Bhatia <rachit.bhatia@ibm.com>
2018-04-07 15:07:04 -0400

    Initial commit with Practice project from web

---
+++ src/main/java/com/idodevjobs/sample/service/ExampleServiceImpl.java
@@ -0,0 +1,12 @@
+package com.idodevjobs.sample.service;
+ 
+import com.idodevjobs.sample.model.ExampleModel;
+import org.springframework.stereotype.Service;
+ 
+@Service("exampleService")
+public class ExampleServiceImpl implements ExampleService {
+	
+	public ExampleModel get(String modelId) {
+        return new ExampleModel("example", 1001);
+    }
+}
